0563ec3465e84d6d723e83c6416c36ed1a3c251b
ghill <ghill@COBD-MXL14042GH.campus.uca.edu>
2023-04-05 09:15:09 -0500

    Updated day18 thread pool example code.

--- src/day18/Driver.java
+++ src/day18/Driver.java
@@ -8,30 +8,38 @@ public class Driver {
 
 	public static void main(String[] args) {
 		try {
-			System.out.println("******** THREADED AND SYNCHRONIZED ******** (won't double up, but will sometimes be backwards)");
-			Ball ball = new Ball();
-			Thread ping = new Thread(new SynchronizedSpeedGame("ping", ball));
-			Thread pong = new Thread(new SynchronizedSpeedGame("pong", ball));
-			ping.start();
-			pong.start();
-			ping.join();
-			pong.join();
-			System.out.println();
-
-			System.out.println("******** COORDINATED, THREADED, AND SYNCHRONIZED ******** (will always work as intended)");
-			Ball ball2 = new Ball(1);
-			ping = new Thread(new CoordinatedSynchronizedSpeedGame("ping", 1, ball2));
-			pong = new Thread(new CoordinatedSynchronizedSpeedGame("pong", 2, ball2));
-			ping.start();
-			pong.start();
-			ping.join();
-			pong.join();
-			System.out.println();
+//			System.out.println("******** THREADED AND SYNCHRONIZED ******** (won't double up, but will sometimes be backwards)");
+//			Ball ball = new Ball();
+//			Thread ping = new Thread(new SynchronizedSpeedGame("ping", ball));
+//			Thread pong = new Thread(new SynchronizedSpeedGame("pong", ball));
+//			ping.start();
+//			pong.start();
+//			ping.join();
+//			pong.join();
+//			System.out.println();
+//
+//			System.out.println("******** COORDINATED, THREADED, AND SYNCHRONIZED ******** (will always work as intended)");
+//			Ball ball2 = new Ball(1);
+//			ping = new Thread(new CoordinatedSynchronizedSpeedGame("ping", 1, ball2));
+//			pong = new Thread(new CoordinatedSynchronizedSpeedGame("pong", 2, ball2));
+//			ping.start();
+//			pong.start();
+//			ping.join();
+//			pong.join();
+//			System.out.println();
 			
-			ThreadPoolExecutor tp = new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+			ThreadPoolExecutor tp = new ThreadPoolExecutor(5, 10, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(5));
 			tp.allowCoreThreadTimeOut(true);
 			for (char i='A';i<='Z';i++) {
-				tp.execute(new Work(i));
+				while (true) {
+					try {
+						// if the queue is full, it will throw a RejectedExecutionException
+						tp.execute(new Work(i));
+						break;
+					} catch (Exception e) {
+						// do nothing, just try again
+					} // end catch
+				} // end while
 			} // end for
 			while (!(tp.getActiveCount() == 0)) {
 				// loop "main" thread endlessly until the work is done, then we can perform cleanup tasks
